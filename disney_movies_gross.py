99609adfba45e882b5391aff820dd2c32fc5df3b
Vijay Stroup <vijaystroup@gmail.com>
2020-02-28 17:37:14 -0500

    To OOP so I dont have to keep passing the dataframe around everywhere

--- disney_movies_gross.py
+++ disney_movies_gross.py
@@ -1,53 +1,64 @@
+"""
+labels: 'movie_title', 'release_date', 'genre', 'MPAA_rating', 'total_gross',
+        'inflation_adjusted_gross'
+"""
+
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-path = os.path.dirname(__file__)
-plot_loc = 'disney_movies_gross_plots'
 plt.style.use('ggplot')
 
 
-def gross_to_int(df):
-    """Change datatype of gross values from string to integer for sorting"""
+class Disney_Movies_Gross:
+    def __init__(self):
+        self.path = os.path.dirname(__file__)
+        self.plot_loc = 'disney_movies_gross_plots'
+        self.df = pd.read_csv(f'{self.path}/disney_movies_total_gross.csv')
 
-    for i in df.iterrows():
-        total_gross = int(
-                i[1]['total_gross'].replace('$', '').replace(',', '')
+    def gross_to_int(self):
+        """Change datatype of gross values from string to integer for sorting"""
+
+        for i in self.df.iterrows():
+            total_gross = int(
+                    i[1]['total_gross'].replace('$', '').replace(',', '')
+                )
+            inflation_adj_gross = int(
+                    i[1]['inflation_adjusted_gross'].replace('$', '').replace(',', '')
+                )
+
+            self.df.replace(
+                to_replace = [i[1]['total_gross'], i[1]['inflation_adjusted_gross']],
+                value = [total_gross, inflation_adj_gross],
+                inplace = True
             )
-        inflation_adj_gross = int(
-                i[1]['inflation_adjusted_gross'].replace('$', '').replace(',', '')
-            )
+    
+    def plot_top5(self):
+        """Save a plot of the top 5 total grossed titles"""
 
-        df.replace(
-            to_replace = [i[1]['total_gross'], i[1]['inflation_adjusted_gross']],
-            value = [total_gross, inflation_adj_gross],
-            inplace = True
-        )
+        df = self.df.sort_values('total_gross', ascending=False)
+        plt.bar(df[:5]['movie_title'], df[:5]['total_gross'])
+
+        plt.xticks(rotation=30, ha='right')
+        plt.title('Top 5 Total Grossing Disney Movies')
+        plt.xlabel('Movie Title')
+        plt.ylabel('Total Gross ($)')
+        plt.tight_layout()
+
+        plt.savefig(f'{self.path}/{self.plot_loc}/top5_by_total_gross.png')
+
+    def best_genre(self):
+        pass
 
 
-def plot_top5(df):
-    """Save a plot of the top 5 total grossed titles"""
-
-    df = df.sort_values('total_gross', ascending=False)
-    plt.bar(df[:5]['movie_title'], df[:5]['total_gross'])
-
-    plt.xticks(rotation=30, ha='right')
-    plt.title('Top 5 Total Grossing Disney Movies')
-    plt.xlabel('Movie Title')
-    plt.ylabel('Total Gross ($)')
-    plt.tight_layout()
-
-    plt.savefig(f'{path}/{plot_loc}/top5_by_total_gross.png')
+    def best_MPAA_rating(self):
+        pass
 
 
 if __name__ == '__main__':
-    df = pd.read_csv(f'{path}/disney_movies_total_gross.csv')
+    disney = Disney_Movies_Gross()
+    disney.gross_to_int()
+    disney.plot_top5()
 
-    # labels
-    df_labels = df.columns
-
-    # print(df[:2]['movie_title'])
-
-    gross_to_int(df)
-    plot_top5(df)
+    print(disney.df['genre'].unique())
