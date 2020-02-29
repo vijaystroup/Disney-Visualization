3cbcbd996160c29ea9fb2536cbce1c5519bd332f
Vijay Stroup <vijaystroup@gmail.com>
2020-02-29 18:50:51 -0500

    best rating

--- disney_movies_gross.py
+++ disney_movies_gross.py
@@ -3,11 +3,8 @@ labels: 'movie_title', 'release_date', 'genre', 'MPAA_rating', 'total_gross',
         'inflation_adjusted_gross'
 """
 
-from collections import OrderedDict
 import os
-import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 plt.style.use('ggplot')
@@ -51,61 +48,76 @@ class Disney_Movies_Gross:
         plt.savefig(f'{self.path}/{self.plot_loc}/top5_by_total_gross.png')
 
     def best_genre(self):
-        """Method to find out Disney's best selling genre of film"""
+        """Method to find out Disney's best selling genre of film. The value of
+        a genre is determined by the mean inflation_adjusted_gross of each
+        genre."""
 
-        genres = {}
-        total = 0
-        for row in self.df.iterrows():
-            try:
-                genre = genres[row[1]['genre']]
-                genres.update(
-                    {row[1]['genre']: (genre[0]+1, genre[1]+row[1]['inflation_adjusted_gross'])}
-                )
-                total += row[1]['inflation_adjusted_gross']
-            except KeyError:
-                genres.update(
-                    {row[1]['genre']: (1, row[1]['inflation_adjusted_gross'])}
-                )
-                total += row[1]['inflation_adjusted_gross']
-
-        # instead of just finding the most money a genre has made, I will compare
-        # the value of the genre based upon its gross earnings to how many titles
-        # in that genre were produced (mean per title in genre)
-        for key in genres:
-            max_value = ('', 0)
-            value = (key, genres[key][1] / genres[key][0])
-            if value[1] > max_value[1]:
-                max_value = value
+        genres, total, labels, values = self.iter_data('genre', 'inflation_adjusted_gross')
 
         # plotting data
-        labels = [f'{key} - {round(genres[key][1] / total * 100, 1)}%' for key in genres]
-        values = [genres[key][1] for key in genres]
-            
         colors = ['yellowgreen','red','gold','lightskyblue','black','lightcoral',
                   'blue','pink', 'darkgreen','yellow','grey','violet','magenta',
                   'cyan','grey']
+        title = 'Disney\'s Genre Percent Value of Gross Income'
+        saveas = 'genre_percent_value'
+
+        self.pie_plot(values, labels, colors, title, saveas)
+
+    def best_MPAA_rating(self):
+        """Method for finding the best selling set of titles with a certain
+        rating. Just like best_genre, we will get the mean of a title in each
+        rating so we can see the real value of each rating"""
+
+        rating, total, labels, values = self.iter_data('MPAA_rating', 'inflation_adjusted_gross')
+        
+        # plotting data
+        colors = ['yellowgreen','red','gold','lightskyblue','black','lightcoral']
+        title = 'Disney\'s MPAA Rating Percent Value of Gross Income'
+        saveas = 'rating_percent_value'
+
+        self.pie_plot(values, labels, colors, title, saveas)
+
+    def iter_data(self, key, value):
+        total = 0
+        data = {}
+        for row in self.df.iterrows():
+            try:
+                datum = data[row[1][key]]
+                data.update(
+                    {row[1][key]: (datum[0]+1, datum[1]+row[1][value])}
+                )
+                total += row[1][value]
+            except KeyError:
+                data.update(
+                    {row[1][key]: (1, row[1][value])}
+                )
+                total += row[1][value]
+        
+        labels = [f'{k} - {round(data[k][1] / total * 100, 1)}%' for k in data]
+        values = [data[k][1] for k in data]
+
+        return (data, total, labels, values)
+
+    def pie_plot(self, values, labels, colors, title, saveas):
         circle = plt.Circle((0, 0), 0.7, color='white')
         wedges, texts = plt.pie(values, colors=colors)
         p = plt.gcf()
         p.gca().add_artist(circle)
 
         wedges, labels = self.sort_labels(wedges, labels, values)
-        plt.legend(wedges, labels, loc='center', fontsize=8)
+        plt.legend(wedges, labels, loc='center', frameon=False, fontsize=7)
 
-        plt.title('Disney\'s Genre Percent Value')
+        plt.title('Disney\'s Genre Percent Value of Gross Income')
         plt.axis('equal')
         plt.tight_layout()
 
-        plt.savefig(f'{self.path}/{self.plot_loc}/genre_percent_value.png')
-
-
-    def best_MPAA_rating(self):
-        pass
+        plt.savefig(f'{self.path}/{self.plot_loc}/{saveas}.png')
 
     @staticmethod
     def sort_labels(classes, labels, y):
         """Method of sorting lables for legend. Algorithm from Saullo G. P. Castro
         on StackOverflow."""
+
         classes, labels, _ = zip(*sorted(
             zip(classes, labels, y),
             key = lambda x: x[2],
@@ -113,6 +125,10 @@ class Disney_Movies_Gross:
         ))
 
         return (classes, labels)
+    
+    @staticmethod
+    def stats():
+        pass
 
 
 if __name__ == '__main__':
@@ -121,5 +137,6 @@ if __name__ == '__main__':
     disney.plot_top5()
     plt.clf() # clear top5 plot for best_genre plot
     disney.best_genre()
+    disney.best_MPAA_rating()
 
-    # print(disney.df['genre'].unique())
+    # print(disney.df['MPAA_rating'].unique())
