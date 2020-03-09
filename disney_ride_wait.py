a7bc6c778278b69497e8fa05139da0b6a86abb28
Vijay Stroup <vijaystroup@gmail.com>
2020-03-08 21:10:51 -0400

    data and test plotting splash mountain

--- disney_ride_wait.py
+++ disney_ride_wait.py
@@ -1,4 +1,13 @@
+import os
+import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('/d/python/disney/data/splash_mountain.csv')
-print(df)
+path = os.path.dirname(__file__)
+df = pd.read_csv(f'{path}/data/splash_mountain.csv')
+df.drop(df[df['SPOSTMIN'] < 0].index, inplace=True)
+df_2019 = df.loc[df['date'].str.contains('2019')]
+print()
+print(df_2019)
+
+plt.plot(df[:50]['datetime'], df[:50]['SPOSTMIN'])
+plt.show()
