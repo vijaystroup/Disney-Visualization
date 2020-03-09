9cf6383c8eaa64dd21f7bfe28c60f4167dc4330e
Vijay Stroup <vijaystroup@gmail.com>
2020-03-09 14:00:42 -0400

    cleaning nan values

--- disney_ride_wait.py
+++ disney_ride_wait.py
@@ -1,13 +1,18 @@
+import math
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
 path = os.path.dirname(__file__)
 df = pd.read_csv(f'{path}/data/splash_mountain.csv')
-df.drop(df[df['SPOSTMIN'] < 0].index, inplace=True)
-df_2019 = df.loc[df['date'].str.contains('2019')]
-print()
-print(df_2019)
 
-plt.plot(df[:50]['datetime'], df[:50]['SPOSTMIN'])
+# clean data
+df.drop(df[df['SPOSTMIN'] < 0].index, inplace=True)
+df.dropna(subset=['SPOSTMIN'], inplace=True)
+
+
+df_2019 = df.loc[df['date'].str.contains('2019')]
+
+plt.plot(df[:10]['datetime'], df[:10]['SPOSTMIN'])
 plt.show()
