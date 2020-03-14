fea1a6ea52e60400378517957d5ae9bfbbc6882c
Vijay Stroup <vijaystroup@gmail.com>
2020-03-13 23:03:40 -0400

    weekly plot

--- disney_ride_wait.py
+++ disney_ride_wait.py
@@ -40,12 +40,13 @@ class Ride:
         mean = self.df._get_numeric_data().mean()['SPOSTMIN']
 
         plt.bar(self.df['datetime'], self.df['SPOSTMIN'])
-        plt.plot([0, len(self.df)], [mean, mean], 'b--')
+        plt.plot([0, len(self.df)], [mean, mean], 'b-', label='mean')
         plt.xticks([0, len(self.df)], ['1/1/2012', '12/31/2019'])
         plt.ylim(0, self.df.max()['SPOSTMIN'])
         plt.xlabel('Date')
         plt.ylabel('Waiting Time (min)')
         plt.title(f'Wait Times for {self.ride}')
+        plt.legend()
         try:
             plt.savefig(f'{path}/{report_dir}/{self._ride}/master_wait_times.png')
         except Exception:
@@ -62,12 +63,13 @@ class Ride:
         mean = df_year._get_numeric_data().mean()['SPOSTMIN']
 
         plt.bar(df_year['datetime'], df_year['SPOSTMIN'])
-        plt.plot([0, len(df_year)], [mean, mean], 'b--')
+        plt.plot([0, len(df_year)], [mean, mean], 'b-', label='mean')
         plt.xticks([0, len(df_year)], ['January', 'December'])
         plt.ylim(0, df_year.max()['SPOSTMIN'])
         plt.xlabel('Date')
         plt.ylabel('Waiting Time (min)')
         plt.title(f'{year} Wait Times for {self.ride}')
+        plt.legend()
         try:
             plt.savefig(f'{path}/{report_dir}/{self._ride}/{year}_wait_times.png')
         except Exception:
@@ -85,31 +87,66 @@ class Ride:
         df_month = self.df.loc[self.df['date'].str.contains(f'{month}/.*/2019')]
         mean = df_month._get_numeric_data().mean()['SPOSTMIN']
 
-        plt.plot(df_month['datetime'], df_month['SPOSTMIN'])
-        plt.plot([0, len(df_month)], [mean, mean], 'b--')
+        plt.bar(df_month['datetime'], df_month['SPOSTMIN'])
+        plt.plot([0, len(df_month)], [mean, mean], 'b-', label='mean')
         plt.xticks([0, len(df_month)], [f'{month}/01/2019', f'{month}/31/2019'])
         plt.ylim(0, df_month.max()['SPOSTMIN'])
         plt.xlabel('Date')
         plt.ylabel('Waiting Time (min)')
         plt.title(f'{month}/2019 Wait Times for {self.ride}')
+        plt.legend()
         try:
             plt.savefig(f'{path}/{report_dir}/{self._ride}/2019-{month}_wait_times.png')
         except Exception:
             os.mkdir(f'{path}/{report_dir}/{self._ride}')
             plt.savefig(f'{path}/{report_dir}/{self._ride}/2019-{month}_wait_times.png')
 
-    def weekly_plot(self):
+    def weekly_plot(self, week):
         """Method for plotting wait times for each week in a month in a given
         year. For simplicity as this is only a test program, only the month
         January in 2019 year will be used
         """
 
-        pass
+        plt.clf()
+
+        beg = int(week[0])+1
+        end = int(week[1])-1
+        if week[0] == '00':
+            week_num = '1'
+        elif week[0] == '07':
+            week_num = '2'
+        elif week[0] == '14':
+            week_num = '3'
+        else:
+            week_num = '4'
+
+        df_week = self.df.loc[
+            (self.df['date'] > f'01/{week[0]}/2019') & (self.df['date'] < f'01/{week[1]}/2019')
+        ]
+        mean = df_week._get_numeric_data().mean()['SPOSTMIN']
+
+        plt.bar(df_week['datetime'], df_week['SPOSTMIN'])
+        plt.plot([0, len(df_week)], [mean, mean], 'b-', label='mean')
+        plt.xticks([0, len(df_week)], [f'01/{beg:02}/2019', f'01/{end:02}/2019'])
+        plt.ylim(0, df_week.max()['SPOSTMIN'])
+        plt.xlabel('Date')
+        plt.ylabel('Waiting Time (min)')
+        plt.title(f'Jan 2019 Week {week_num} Wait Times for {self.ride}')
+        plt.legend()
+        try:
+            plt.savefig(
+                f'{path}/{report_dir}/{self._ride}/2019-01-week{week_num}_wait_times.png'
+            )
+        except Exception:
+            os.mkdir(f'{path}/{report_dir}/{self._ride}')
+            plt.savefig(
+                f'{path}/{report_dir}/{self._ride}/2019-01-week{week_num}_wait_times.png'
+            )
 
     def daily_plot(self):
         """Method for plotting wait times for each day in a week for a month in
         a given year. For simplicity as this is only a test program, only the
-        day Saturday in the month January in 2019 year will be used
+        first week in the month January in 2019 year will be used
         """
 
         pass
@@ -125,12 +162,18 @@ class Ride:
         months = [
             '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'
         ]
+        weeks = [
+            ['00','08'], ['07','15'], ['14','22'], ['21','32']
+        ]
 
         with concurrent.futures.ProcessPoolExecutor() as executor:
             if plot_type == 'yearly_plot':
                 executor.map(self.yearly_plot, years)
             elif plot_type == 'monthly_plot':
                 executor.map(self.monthly_plot, months)
+            elif plot_type == 'weekly_plot':
+                executor.map(self.weekly_plot, weeks)
+
 
 
 def best_route():
@@ -145,4 +188,5 @@ if __name__ == '__main__':
     ride_splash = Ride('splash_mountain')
     # ride_splash.master_plot()
     # ride_splash.multi_process('yearly_plot')
-    ride_splash.multi_process('monthly_plot')
+    # ride_splash.multi_process('monthly_plot')
+    ride_splash.multi_process('weekly_plot')
